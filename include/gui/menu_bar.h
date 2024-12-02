171ec08a88ed32ae0ec68cbc65139fc0b99a9a7f
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2024-12-01 19:18:51 -0600

    Update menu_bar.h

--- include/gui/menu_bar.h
+++ include/gui/menu_bar.h
@@ -1,1 +1,9 @@
+#ifndef MENU_BAR_H
+#define MENU_BAR_H
 
+#include <gtk/gtk.h>
+
+// Function to create the menu bar
+GtkWidget* create_menu_bar(GtkApplication *app, GtkWindow *parent_window);
+
+#endif // MENU_BAR_H
