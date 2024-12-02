fa48bd97c253a4bf4c46763cc3e14ad6529251fa
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2024-12-01 19:17:52 -0600

    Update main_window.h

--- include/gui/main_window.h
+++ include/gui/main_window.h
@@ -1,1 +1,9 @@
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
 
+#include <gtk/gtk.h>
+
+// Function to create the main window
+void create_main_window(GtkApplication *app);
+
+#endif // MAIN_WINDOW_H
