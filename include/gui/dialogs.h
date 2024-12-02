584d5c57e5cbaf4dd0166f38ff1df087664f823c
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2024-12-01 19:18:21 -0600

    Update dialogs.h

--- include/gui/dialogs.h
+++ include/gui/dialogs.h
@@ -1,1 +1,12 @@
+#ifndef DIALOGS_H
+#define DIALOGS_H
 
+#include <gtk/gtk.h>
+
+// Function to show the About dialog
+void show_about_dialog(GtkWindow *parent_window);
+
+// Function to show the Preferences dialog
+void show_preferences_dialog(GtkWindow *parent_window);
+
+#endif // DIALOGS_H
